995d752c02cf6b081e536d80c1c04f9683f250c3
Vaishnavi-shree <vaishnavi09gecv@gmail.com>
2026-09-09 11:34:52 +0530

    simplecon

---
+++ Simple Conditions/1.py
@@ -0,0 +1,9 @@
+# Take a number and print whether it’s positive, negative, or zero
+
+number = float(input("enter a number :  "))
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
