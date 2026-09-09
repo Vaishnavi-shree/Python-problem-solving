8710b35aa8fd9229119d40e22e63a5b3133b812d
Vaishnavi-shree <vaishnavi09gecv@gmail.com>
2026-09-09 11:56:12 +0530

    updated

--- Simple Conditions/3.py
+++ Simple Conditions/3.py
@@ -1,6 +1,8 @@
-# Check if a number is even or odd. 
-number = int(input("Enter a number: "))
-if number % 2 == 0:
-    print("The number is even.")
-else:
-    print("The number is odd.")
+# Check if a number is divisible by 5.
+
+num = int(input("give your no."))
+if (num % 5 == 0 ):
+  print("No. is divisible by 5")
+else :
+  print("No.is not divisible")
+  
