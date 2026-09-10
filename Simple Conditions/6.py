aca183b528fff37f9b10aa2423b1e105664c2f28
Vaishnavi-shree <vaishnavi09gecv@gmail.com>
2026-09-10 21:27:29 +0530

    todays work

---
+++ Simple Conditions/6.py
@@ -0,0 +1,9 @@
+# Take two numbers and print the larger one.
+
+num_1 = float(input("Enter no. first"))
+num_2 = float(input("Enter no. second"))
+
+if (num_1>num_2):
+    print(num_1)
+else:
+    print(num_2)
