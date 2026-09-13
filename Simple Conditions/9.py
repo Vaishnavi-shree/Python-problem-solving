58b8880e67a56f058f11bd9f7bb4a1da67b3d64e
Vaishnavi-shree <vaishnavi09gecv@gmail.com>
2026-09-13 15:31:08 +0530

    todays work

---
+++ Simple Conditions/9.py
@@ -0,0 +1,8 @@
+# Take a character and check if it’s a vowel or consonant
+
+char = str(input("Enter an Alphabet"))
+if (char == 'a') or (char == 'e') or (char == 'o') or (char == 'i') or (char == 'u') or (char == 'A') or (char == 'E') or (char == 'I') or (char == 'O') or (char == 'U') :
+    print("Its a Vowel")
+else:
+    print("Its a consonant")
+
