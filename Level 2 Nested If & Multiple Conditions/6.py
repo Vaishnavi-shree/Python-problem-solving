e7a0887582f267cc55f162d1df1b71a878e6cdd0
Vaishnavi-shree <vaishnavi09gecv@gmail.com>
2026-09-18 21:18:32 +0530

    todays work

---
+++ Level 2 Nested If & Multiple Conditions/6.py
@@ -0,0 +1,15 @@
+# Take marks (0–100) and print the corresponding grade (A/B/C/D/F)
+
+# 1. Take three sides and check if they form a valid triangle.
+
+def is_valid_triangle(a, b, c):
+    # Check if the sum of any two sides is greater than the third side
+    if (a + b > c) and (a + c > b) and (b + c > a):
+        return True
+    return False
+
+# Test cases
+print(is_valid_triangle(7, 10, 5))  # Returns True (Valid)
+print(is_valid_triangle(1, 10, 2))  # Returns False (Invalid)
+
+
